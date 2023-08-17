87771aacb9805eb2d9cc21cf888d8ac5c15aaef5
Vibhor Kumar <vibhork85@gmail.com>
2023-08-17 11:02:50 +0530

    Login.js Updated

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,19 +1,40 @@
 import "./Login.scss";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const Login = () => {
   const inputEmail = useRef();
   const inputPassword = useRef();
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [isPassValid, setIsPassValid] = useState(false);
 
-    
-    
+  const onSubmitHandelar = (event) => {
+    event.preventDefault();
+    const email = inputEmail.current.value;
+    const password = inputPassword.current.value;
+    if (email.trim() === "") {
+      setIsEmailValid(true);
+      return;
+    }
+    if (password === "") {
+      setIsPassValid(true);
+      return;
+    }
+    event.target.reset();
+    console.log(email, password);
+  };
+
+  const invalidEmail = isEmailValid
+    ? "form-control is-invalid"
+    : "form-control";
+  const invalidPass = isPassValid ? "form-control is-invalid" : "form-control";
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={onSubmitHandelar}>
       <div className="form__div">
         <label className="form-label">Login</label>
         <input
-          className="form-control"
+          className={invalidEmail}
           type="email"
           placeholder="Enter Email"
           ref={inputEmail}
@@ -22,7 +43,7 @@ const Login = () => {
       <div className="form__div">
         <label className="form-label">Password</label>
         <input
-          className="form-control"
+          className={invalidPass}
           type="password"
           placeholder="Enter Password"
           ref={inputPassword}
